5097701401e3998969f2a48bdc89e4e819735ba3
Tom Lazar <tomster@pyfidelity.com>
2013-05-17 17:32:04 +0200

    factor out the mixin tests

--- ezjaildeploy/tests/test_stages.py
+++ ezjaildeploy/tests/test_stages.py
@@ -2,36 +2,7 @@ from collections import OrderedDict
 from mock import MagicMock, patch
 from pytest import fixture
 from ezjaildeploy.api import BaseJail
-from ezjaildeploy.stages import NameDescription, Step, Stage
-
-
-class DummyNameDescription(NameDescription):
-    '''You can provide the description as a doc string '''
-
-    name = 'dummy'
-
-
-@fixture
-def name_description():
-    return NameDescription()
-
-
-def test_stage_explicit_name():
-    bootstrap = NameDescription(name='bootstrap')
-    assert bootstrap.name == 'bootstrap'
-
-
-def test_stage_default_name(name_description):
-    assert name_description.name == NameDescription.name
-
-
-def test_stage_explicit_description():
-    stage = NameDescription(description='''Nothing to see here''')
-    assert stage.description == '''Nothing to see here'''
-
-
-def test_stage_docstring_description(name_description):
-    assert name_description.description == NameDescription.__doc__
+from ezjaildeploy.stages import Step, Stage
 
 
 @fixture
